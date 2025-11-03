95915dbeb8f130b462652bb22452784835b960f5
DK Kang <devkade12@gmail.com>
2025-11-03 15:56:57 +0900

    feat: add POST /api/auth/login/ endpoint for user authentication

---
+++ PhotoBlogServer/mysite/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for mysite project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
+from blog import views
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register('Post', views.BlogImages)
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('api/auth/login/', views.login, name='api-login'),
+    path('api_root/', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api-token-auth/', obtain_auth_token),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
